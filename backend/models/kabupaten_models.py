493126d31e6a787843af3f615bb17ab03f10b971
SeinalArifin149 <arifinseinals3@gmail.com>
2026-05-16 00:09:15 +0700

    fix(kabupaten  models)

--- backend/models/kabupaten_models.py
+++ backend/models/kabupaten_models.py
@@ -1,14 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 
+# Cukup di sini saja db dibuat
 db = SQLAlchemy()
 
 class Kabupaten(db.Model): 
     __tablename__ = "kabupaten"
 
     id_kabupaten = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    
-    produsen_list = db.relationship('produsen',backref='kabupaten_relasi',lazy=True)
     nama_kabupaten = db.Column(db.String(100), nullable=False)
+    
+    # PERBAIKAN: Ganti 'produsen' jadi 'Produsen' (Sesuai nama Class)
+    produsen_list = db.relationship('Produsen', backref='kabupaten_relasi', lazy=True)
 
     def to_dict(self):
         return {

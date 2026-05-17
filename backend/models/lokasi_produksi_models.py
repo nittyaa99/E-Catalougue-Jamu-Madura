5fbf148ba2d790888aa81d5fe77974e9ae47b5c5
SeinalArifin149 <arifinseinals3@gmail.com>
2026-05-18 00:19:00 +0700

    fix(models lokasi_produksi):typo in lokasi-produksi change to lokasi_pro

--- backend/models/lokasi_produksi_models.py
+++ backend/models/lokasi_produksi_models.py
@@ -1,24 +1,21 @@
-# models/lokasi_produksi_models.py
-from models.kabupaten_models import db # <--- AMBIL DARI PUSAT
+from models.kabupaten_models import db 
 
 class LokasiProduksi(db.Model): 
     __tablename__ = "lokasi_produksi"
 
     id_lokasi_produksi = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nama_lokasi_produksi = db.Column(db.String(100), nullable=False)
+    
+    # 🔥 INI YANG BIKIN ERROR TADI (Udah diganti jadi nama_lokasi)
+    nama_lokasi = db.Column(db.String(100), nullable=False) 
 
     id_kabupaten = db.Column(db.Integer, db.ForeignKey('kabupaten.id_kabupaten'), nullable=True)
 
-    # FIX: Ganti 'kabupaten' jadi 'Kabupaten' (Pake Huruf Besar)
-    kabupaten_relasi = db.relationship(
-        'Kabupaten', 
-        backref='lokasi_produksi_relasi'
-    )
+    kabupaten_relasi = db.relationship('Kabupaten', backref='lokasi_produksi_relasi')
 
     def to_dict(self):
         return {
             "id_lokasi_produksi": self.id_lokasi_produksi,
-            "nama_lokasi_produksi": self.nama_lokasi_produksi, 
+            "nama_lokasi": self.nama_lokasi,  # <--- Manggilnya juga nama_lokasi
             "id_kabupaten": self.id_kabupaten,
             "nama_kabupaten": self.kabupaten_relasi.nama_kabupaten if self.kabupaten_relasi else None
         }
